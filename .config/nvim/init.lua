85b18ca0138654a08d0a43ce8aeec037d0f8f1a0
ShubhamTiwary914 <shubhamtiwary914@gmail.com>
2026-02-10 14:49:34 +0530

    sync - archz new install - quickshell everblush ricing v2

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,13 +1,48 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- VIM OPTIONS --------------------- 
+vim.cmd("set expandtab")
+vim.cmd("set relativenumber")
+vim.cmd("set tabstop=4")
+vim.cmd("set softtabstop=4")
+vim.cmd("set shiftwidth=4")
 
-vim.cmd([[
-  augroup TransparentBG
-    autocmd!
-    autocmd VimEnter * hi Normal ctermbg=none guibg=none
-    autocmd VimEnter * hi NormalFloat ctermbg=none guibg=none
-    autocmd VimEnter * hi SignColumn ctermbg=none guibg=none
-    autocmd VimEnter * hi LineNr ctermbg=none guibg=none
-    autocmd VimEnter * hi EndOfBuffer ctermbg=none guibg=none
-  augroup END
-]])
+vim.g.mapleader = ' ' 
+vim.g.maplocalleader = ' ' 
+
+
+-- LAZY.NVIM & PLUGINS --------------------- 
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+end
+vim.opt.rtp:prepend(lazypath)
+
+local opts = {}
+require("lazy").setup("plugins", opts)
+
+
+-- CUSTOM STUFF --------------------- 
+local function require_dir(modpath)
+  local files = vim.fn.globpath(
+    vim.fn.stdpath("config") .. "/lua/" .. modpath:gsub("%.", "/"),
+    "*.lua",
+    false,
+    true
+  )
+
+  for _, f in ipairs(files) do
+    local name = f:match("([^/]+)%.lua$")
+    require(modpath .. "." .. name)
+  end
+end
+-- loads the lua files from ~/.config/nvim/lua/custom 
+require_dir("custom")
---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,80 @@
+local lspConfigs = function ()
+    vim.lsp.config['lua_ls'] = {
+        cmd = { 'lua-language-server' },
+        filetypes = { 'lua' },
+    }
+    vim.lsp.enable('lua_ls')
+
+    vim.lsp.config['python_ls'] = {
+        cmd = {'pyright'},
+        filetypes = {'py'},
+    }
+    vim.lsp.enable('python_ls')
+
+    vim.lsp.config['bash_ls'] = {
+        cmd = {'bash-language-server'},
+        filetypes = {'sh'},
+    }
+    vim.lsp.enable('bash_ls')
+
+    vim.lsp.config['javascript_ls'] = { 
+        cmd = {'quick-lint-js'},
+        filetypes = {'js'},
+    }
+    vim.lsp.enable('javascript_ls')
+
+
+    vim.lsp.config['typescript_ls'] = {
+        cmd = {'typescript_language-server'},
+        filetypes = {'ts'},
+    } 
+    vim.lsp.enable('typescript_ls')
+
+    vim.lsp.config['golang_ls'] = {
+        cmd = {'gopls'},
+        filetypes = {'go'},
+    } 
+    vim.lsp.enable('golang_ls')
+
+    vim.lsp.config['docker_ls'] = {
+        cmd = {'docker-language-server'},
+        filetypes = {'Dockerfile', 'yml', 'yaml'},
+    }
+    vim.lsp.enable('docker_ls')
+end
+
+
+
+return {
+   --mason for downloading LSP servers, Lint, Formatter, ..
+    {
+        "mason-org/mason.nvim",
+    }, 
+    --connects mason & LSP config
+    -- :LspInfo for LSP list & logs
+    {
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        },
+        config = function()
+            require("mason-lspconfig").setup {
+                ensure_installed = { "lua_ls" },
+            }
+        end
+    },
+    -- LSP config for neovim
+    -- LSP servers list: https://neovim.io/doc/user/lsp.html#lsp-quickstart 
+    -- How to config: https://neovim.io/doc/user/lsp.html#lsp-quickstart
+    -- Check status, logs for LSP -> :LspInfo
+    {
+        "neovim/nvim-lspconfig",
+        config = function()
+            vim.keymap.set('n','hh', vim.lsp.buf.hover, {}) -- hover info details
+            vim.keymap.set('n','hj', vim.lsp.buf.implementation, {}) -- goto implementation 
+            vim.keymap.set('n','hg', vim.lsp.buf.code_action, {}) -- suggestions on fixes (via treesitter-select)
+            lspConfigs()
+        end
+    }
+}
